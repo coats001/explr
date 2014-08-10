a1ae1ae64bb8ccaeca916b84464bad2b25450960
Amos Coats <coats01@hotmail.com>
2014-08-10 15:22:23 +0200

    enhanced

--- plot4.R
+++ plot4.R
@@ -13,7 +13,7 @@ with(consumption, {
   plot(Time,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
   lines(Time,Sub_metering_2,type="l", col="red")
   lines(Time,Sub_metering_3,type="l", col="blue")
-  legend("topright",pch="-",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.8)
+  legend("topright",pch="-",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty = "n")
   plot(Time,Global_reactive_power,type="n", xlab="datetime")
   lines(Time,Global_reactive_power,type="l")
 })
